02f5d5f53ca78f4acffbe1ebcfc2aa4dd5630a5c
zeph1re <arlinnog1122@gmail.com>
2022-10-23 14:55:44 +0700

    add pindah button

--- app/src/main/java/and5/finalproject/event/MainActivity.kt
+++ app/src/main/java/and5/finalproject/event/MainActivity.kt
@@ -1,20 +1,41 @@
 package and5.finalproject.event
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.SnackbarContentLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private var textView : TextView = findViewById(R.id.textView) as TextView
+    private lateinit var buttonToast: Button
+    private lateinit var buttonSnackbar: Button
+    private lateinit var buttonPindah: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView.setOnClickListener{
+        buttonToast = findViewById(R.id.button_toast)
+        buttonSnackbar = findViewById(R.id.button_snackbar)
+        buttonPindah = findViewById(R.id.button_pindah)
+
+        buttonToast.setOnClickListener{
             Toast.makeText(this, "Button di klik", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonSnackbar.setOnClickListener {
+            Snackbar.make(it, "Button di klik", Snackbar.LENGTH_SHORT).show()
+        }
+
+        buttonPindah.setOnClickListener {
+            //Untuk berpindah antar activity
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("nama orang", "Pakde Joko")
+            startActivity(intent)
         }
 
 
--- app/src/main/java/and5/finalproject/event/SecondActivity.kt
+++ app/src/main/java/and5/finalproject/event/SecondActivity.kt
@@ -1,11 +1,24 @@
 package and5.finalproject.event
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.Secure.getString
+import android.provider.Settings.System.getString
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var textFromActivity1 : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        textFromActivity1 = findViewById(R.id.text_activity2)
+
+        val data = intent.getStringExtra("nama orang")
+
+        textFromActivity1.text = data
     }
 }
