c6881157bb4d3e139c56455d34cb8efe6bca6a40
zeph1re <arlinnog1122@gmail.com>
2022-10-23 14:55:31 +0700

    add pindah button

---
+++ app/src/main/java/and5/finalproject/event/SecondActivity.kt
@@ -0,0 +1,11 @@
+package and5.finalproject.event
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
